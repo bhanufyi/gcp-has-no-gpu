22fcb5599de91f98d230ce40120d362a4f00e74a
bhanufyi <bhanuprasadcherukuvada2000@gmail.com>
2024-12-15 01:05:18 -0500

    feat: try to setup compute engine with gpu 🥵

---
+++ vm.tf
@@ -0,0 +1,63 @@
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "bhanu-gpu" {
+  boot_disk {
+    auto_delete = false
+    device_name = "bhanu-gpu"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20241210"
+      size  = 20
+      type  = "pd-standard"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  guest_accelerator {
+    count = 1
+    type  = "nvidia-tesla-t4"
+  }
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "n1-standard-4"
+  name         = "bhanu-gpu"
+
+  network_interface {
+    access_config {
+      network_tier = "STANDARD"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "TERMINATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "922801875648-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "europe-west2-b"
+}
